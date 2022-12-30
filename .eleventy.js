de3771e56cecaac0f82271dbcc0cbaab4169458f
wingsooyoung <hsm_rox_16@hotmail.com>
2022-12-30 17:00:46 -0500

    Make changes

--- .eleventy.js
+++ .eleventy.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
             output: "dist",
             includes: "_includes"
         },
-        templateFormats : ["hbs", "md"],
+        templateFormats : ["hbs", "md", "html"],
         htmlTemplateEngine : ["hbs"],
         markdownTemplateEngine : ["hbs"]
     };
