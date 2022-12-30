6924d03d83168d0361f19c019cbccb76a8d56d7e
wingsooyoung <hsm_rox_16@hotmail.com>
2022-12-30 00:14:16 -0500

    Make changes

--- src/site/_data/letters.js
+++ src/site/_data/letters.js
@@ -25,24 +25,24 @@ module.exports = () => {
     //         letters[entry.data.path] = [letter];
     //     }
     // });
-
-    for (let entry in submissions) {
-        let letter = {
-            // avatar: gravatar.url(entry.data.email, {s: '100', r: 'x', d: 'retro'}, true),
-            // signature nickname message
-            path: entry.data.path,
-            signature: entry.data.signature,
-            nickname: entry.data.nickname,
-            message: entry.data.message.trim()
-        };
-
-        // Add it to an existing array or create a new one in the comments object
-        if (letters[entry.data.path]) {
-            letters[entry.data.path].push(letter);
-        } else {
-            letters[entry.data.path] = [letter];
-        }
-    }
+    //
+    // for (let entry in submissions) {
+    //     let letter = {
+    //         // avatar: gravatar.url(entry.data.email, {s: '100', r: 'x', d: 'retro'}, true),
+    //         // signature nickname message
+    //         path: entry.data.path,
+    //         signature: entry.data.signature,
+    //         nickname: entry.data.nickname,
+    //         message: entry.data.message.trim()
+    //     };
+    //
+    //     // Add it to an existing array or create a new one in the comments object
+    //     if (letters[entry.data.path]) {
+    //         letters[entry.data.path].push(letter);
+    //     } else {
+    //         letters[entry.data.path] = [letter];
+    //     }
+    // }
 
     return letters;
 
