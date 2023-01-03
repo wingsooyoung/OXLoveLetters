afea330b0c808efc55e043a9a04af2b00932a477
wingsooyoung <hsm_rox_16@hotmail.com>
2023-01-02 19:30:14 -0500

    Make changes

--- src/functions/form-action.js
+++ src/functions/form-action.js
@@ -27,7 +27,7 @@ function purgeComment(id) {
 /*
   Handle the lambda invocation
 */
-export function handler(event, context, callback) {
+exports.handler = function (event, context, callback) {
 
     // parse the payload
     var body = event.body.split("payload=")[1];
