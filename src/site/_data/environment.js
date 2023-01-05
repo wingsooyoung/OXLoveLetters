a72cde13d10c354b56b72919f802cb9cfe08b033
wingsooyoung <hsm_rox_16@hotmail.com>
2023-01-04 21:28:34 -0500

    Make changes

--- src/site/_data/environment.js
+++ src/site/_data/environment.js
@@ -1,7 +1,7 @@
 // Make it easier for us to provide some UI logic
 // based on whether environment variables have been initialised
 
-module.exports = () => {
+export default () => {
 
     const {
         NETLIFY_AUTH_TOKEN,
@@ -10,10 +10,10 @@ module.exports = () => {
     } = process.env;
 
     return {
-        ready : NETLIFY_AUTH_TOKEN && SLACK_WEBHOOK_URL ? true : false,
-        NETLIFY_AUTH_TOKEN_ready : NETLIFY_AUTH_TOKEN ? true : false,
-        SLACK_WEBHOOK_URL_ready : SLACK_WEBHOOK_URL ? true : false,
+        ready : !!(NETLIFY_AUTH_TOKEN && SLACK_WEBHOOK_URL),
+        NETLIFY_AUTH_TOKEN_ready : !!NETLIFY_AUTH_TOKEN,
+        SLACK_WEBHOOK_URL_ready : !!SLACK_WEBHOOK_URL,
         SITE_NAME
     }
 
-};
+}
