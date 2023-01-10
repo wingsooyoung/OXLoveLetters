2d4e3ea7a56d072900272dda4afd6dbc80b37936
wingsooyoung <hsm_rox_16@hotmail.com>
2023-01-09 20:59:43 -0500

    Make changes

--- src/site/_data/sheetstest.js
+++ src/site/_data/sheetstest.js
@@ -30,38 +30,46 @@ module.exports = () => {
 
         console.log(`Requesting data from ${googleSheetUrl}`);
 
-        axios.get(googleSheetUrl)
-            .then(response => {
-
-                // massage the data from the Google Sheets API into
-                // a shape that will more convenient for us in our SSG.
-                //response.values[i][j] i=submission j=value
-
-                // var data = {
-                //     "East": [],
-                //     "West": []
-                // };
-                //
-                // response.data.feed.entry.forEach(item => {
-                //     data[item.gsx$conference.$t].push({
-                //         "name": item.gsx$name.$t,
-                //         "team": item.gsx$team.$t
-                //     })
-                // });
-
-                // stash the data locally for developing without
-                // needing to hit the API each time.
-                // seed(JSON.stringify(data), `$../dev/sheet.json`);
-                console.log(response);
-                // resolve the promise and return the data
-                resolve(response);
-
+        axios
+            .get(`${googleSheetUrl}`, {
+                responseType: "json",
             })
-
-            // uh-oh. Handle any errrors we might encounter
-            .catch(error => {
-                console.log('Error :', error);
-                reject(error);
+            .then(function (response) {
+                console.log(response.data);
             });
+
+        // axios.get(googleSheetUrl)
+        //     .then(response => {
+        //
+        //         // massage the data from the Google Sheets API into
+        //         // a shape that will more convenient for us in our SSG.
+        //         //response.values[i][j] i=submission j=value
+        //
+        //         // var data = {
+        //         //     "East": [],
+        //         //     "West": []
+        //         // };
+        //         //
+        //         // response.data.feed.entry.forEach(item => {
+        //         //     data[item.gsx$conference.$t].push({
+        //         //         "name": item.gsx$name.$t,
+        //         //         "team": item.gsx$team.$t
+        //         //     })
+        //         // });
+        //
+        //         // stash the data locally for developing without
+        //         // needing to hit the API each time.
+        //         // seed(JSON.stringify(data), `$../dev/sheet.json`);
+        //         console.log(response);
+        //         // resolve the promise and return the data
+        //         resolve(response);
+        //
+        //     })
+        //
+        //     // uh-oh. Handle any errrors we might encounter
+        //     .catch(error => {
+        //         console.log('Error :', error);
+        //         reject(error);
+        //     });
     })
 }
