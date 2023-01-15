a7d0c18db795abb4590b8c3c5ba6825d2fc5f3b7
wingsooyoung <hsm_rox_16@hotmail.com>
2023-01-15 06:20:28 +0000

    CSS changes

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
         return `<footer>
         <div class="identity-overlay"><div data-netlify-identity-button></div></div>
         <p><small><u>2022 </u><i class="ph-copyright"></i> <a href="https://twitter.com/oxmasterlist">OXMasterlist</a>
-        / <a href="https://omegaxmasterlist.tumblr.com">omegaxmasterlist</a> <a href="/admin/approveletters" style="text-decoration: none;"><i class="ph-key"></i></a>
+        / <a href="https://omegaxmasterlist.tumblr.com">omegaxmasterlist</a> <a href="/admin/approveletters" style="text-decoration: none;"><i class="ph-key-bold"></i></a>
         </small></p>
         </footer>`;
     });
