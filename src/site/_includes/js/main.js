ac8e97a6dfb0d10ce8f828219fdbdb169b7255ed
wingsooyoung <hsm_rox_16@hotmail.com>
2023-01-18 22:36:17 -0500

    Make changes

--- src/site/_includes/js/main.js
+++ src/site/_includes/js/main.js
@@ -33,8 +33,8 @@ function modalZoom(x) {
     modal.style.display = "block";
     let m = document.getElementById("modal1").children;
     let y = x.children;
-    m["contentboxM"].textContent = y[1].children["contentbox"].innerHTML;
-    m["nameboxM"].textContent = y[2].children["namebox"].innerHTML;
+    m["contentboxM"].textContent = y[1].innerHTML;
+    m["nameboxM"].textContent = y[2].innerHTML;
 }
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
