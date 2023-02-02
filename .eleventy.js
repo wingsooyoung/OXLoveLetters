5ba3cc2e0df245c819a216e688e0fb79148f673f
wingsooyoung <hsm_rox_16@hotmail.com>
2023-02-02 17:38:54 -0500

    Make changes

--- .eleventy.js
+++ .eleventy.js
@@ -13,10 +13,10 @@ module.exports = function(config) {
     config.addShortcode("footer", function() {
         return `<footer>
         <p id="disclaimer"><small><em class="show">DISCLAIMER</em></small></p>
-        <div class="identity-overlay"><div data-netlify-identity-button></div></div>
         <p id="line1"><a href="/admin/approveletters" style="text-decoration: none;"><i class="ph-key-bold"></i></a> <u> 2023 </u><i class="ph-copyright"></i> <a href="https://twitter.com/oxmasterlist">OXMasterlist</a>
         / <a href="https://omegaxmasterlist.tumblr.com">omegaxmasterlist</a> <span>&bull; <a href="/pages/sitemap">SITEMAP</a> &bull; <a href="/pages/faq">FAQ</a> &bull; <a href="/pages/aboutthisproject/#contact">CONTACT ME</a></span>
         </p>
+        <div class="identity-overlay"><div data-netlify-identity-button></div></div>
         </footer>`;
     });
 
