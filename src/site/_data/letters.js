8a733ce634c3875595d0173c00850d538955cc7f
wingsooyoung <hsm_rox_16@hotmail.com>
2023-02-25 19:40:17 -0500

    Make changes

--- src/site/_data/letters.js
+++ src/site/_data/letters.js
@@ -1,23 +1,27 @@
 // massage the approved letters data into the shape we'd like
 
 const submissions = require('./letter-form_submissions.json');
+const approved = require('./approved-letters_submissions.json');
 
 module.exports = () => {
     let letters = {};
+
     for(var i = 0; i < submissions.length; i++) {
-        let entry = submissions[i]
+        let entry = submissions[i];
         let letter = {
+            type: 'submission',
             signature: entry.data.signature,
             nickname: entry.data.nickname,
             message: entry.data.message
         };
         // Add it to an existing array or create a new one in the comments object
-        if(letters[entry.data.path]){
-            letters[entry.data.path].push(letter);
-        } else {
-            letters[entry.data.path] = [letter];
-        }
+        // if(letters[entry.data.path]){
+        //     letters[entry.data.path].push(letter);
+        // } else {
+        //     letters[entry.data.path] = [letter];
+        // }
     }
+
     return letters;
 };
 
