dbb6859005ec492abdea36262d2d278b8aeea928
wingsooyoung <hsm_rox_16@hotmail.com>
2023-02-26 19:20:42 -0500

    Make changes

--- src/site/_data/exportSubmissions.js
+++ src/site/_data/exportSubmissions.js
@@ -8,12 +8,51 @@ module.exports = () => {
 
     for(var i = 0; i < submissions.length; i++) {
         let entry = submissions[i];
+        let postcardLink = '';
+        if(entry.data.members === 'Jaehan'){
+            postcardLink = '/assets/jaehanpostcard.svg'
+        }
+        if(entry.data.members === 'Hwichan') {
+            postcardLink = '/assets/hwichanpostcard.svg'
+        }
+        if(entry.data.members === 'Sebin') {
+            postcardLink = '/assets/sebinpostcard.svg'
+        }
+        if(entry.data.members === 'Hangyeom') {
+            postcardLink = '/assets/hangyeompostcard.svg'
+        }
+        if(entry.data.members === 'Taedong') {
+            postcardLink = '/assets/taedongpostcard.svg'
+        }
+        if(entry.data.members === 'Xen') {
+            postcardLink = '/assets/xenpostcard.svg'
+        }
+        if(entry.data.members === 'Jehyun') {
+            postcardLink = '/assets/jehyunpostcard.svg'
+        }
+        if(entry.data.members === 'Kevin') {
+            postcardLink = '/assets/kevinpostcard.svg'
+        }
+        if(entry.data.members === 'Junghoon') {
+            postcardLink = '/assets/junghoonpostcard.svg'
+        }
+        if(entry.data.members === 'Hyuk') {
+            postcardLink = '/assets/hyukpostcard.svg'
+        }
+        if(entry.data.members === 'Yechan') {
+            postcardLink = '/assets/yechanpostcard.svg'
+        }
+        else{
+            postcardLink = '/assets/postcard%20handdrawn%201.svg'
+        }
+
         let letter = {
             type: 'preview',
             signature: entry.data.signature,
             nickname: entry.data.nickname,
             message: entry.data.lettercontent,
-            member: entry.data.members
+            member: entry.data.members,
+            src: postcardLink
         };
         // Add it to an existing array or create a new one in the comments object
         if(previews[entry.data.path]){
