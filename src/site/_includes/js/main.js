d1bcb561c4009fe7c39c36e5ef9f4e23752f4eea
wingsooyoung <hsm_rox_16@hotmail.com>
2023-04-10 22:44:11 -0400

    Make changes

--- src/site/_includes/js/main.js
+++ src/site/_includes/js/main.js
@@ -181,11 +181,11 @@ function filterToggle() {
     if (x.style.visibility === "hidden" && x2.style.visibility === "hidden") {
         x.style.visibility = "visible";
         x2.style.visibility = "visible";
-        y.innerHTML = "FILTER -";
+        y.innerHTML = "FILTER <i class=\"ph ph-minus\"></i>";
     } else {
         x.style.visibility = "hidden";
         x2.style.visibility = "hidden";
-        y.innerHTML = "FILTER +";
+        y.innerHTML = "FILTER <i class=\"ph ph-plus\"></i>";
     }
 }
 
