d6fcaf9197d833f2a8a7f8353cb8a910e33f8ba5
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-20 15:53:40 -0400

    Make changes - submit and delete admin page functions

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,37 +1,51 @@
 //trigger on submission to approved-letters
 
+//need to send deletion-griddy ids through to this function
+    //then call the DELETE form submission api for each one
+        //return result (success message or error message) to the browser
+
+//data received: array of submish-ids
+//data sent: res or error
+
 // populate environment variables locally.
 require('dotenv').config();
 const {
     NETLIFY_AUTH_TOKEN
 } = process.env;
 
-function purgeComment(id) {
-    var url = `https://api.netlify.com/api/v1/forms/639fa0208e1bce0008b4b819/submissions?access_token=${NETLIFY_AUTH_TOKEN}/${id}`;
+//ONLY RUN THE PURGE CALL WHEN DELETION-GRIDDY REACHES 10 LETTERS..???? or something
 
-    fetch(url, {method: "DELETE"})
+function purgeComment(formID, id) {
+    var url = `https://api.netlify.com/api/v1/forms/${formID}/submissions?access_token=${NETLIFY_AUTH_TOKEN}/${id}`;
+
+    return fetch(url, {method: "DELETE"})
         .then(r => console.log(r))
         .then(() => console.log("deletion successful!"))
         .catch((error) => console.log("Error Message: " + error));
 }
-
 //---------------------------------------------------------------------
 exports.handler = async function (event, context, callback) {
+    // Get data off the event body
+    const {
+        theFormID,
+        letterIDs
+    } = JSON.parse(event.body)
 
-    // get the arguments from the notification
+    // letterIDs.forEach(purgeComment(theFormID))
+    for (const thisID of letterIDs) {
+        await purgeComment(theFormID, thisID)
+    }
+
+
+    //below here is just log checking!
+
     var bodyAlt = JSON.parse(event.body);
 
-    // parse the payload
-    var body = event.body.split("payload=")[1];
-    var payload = JSON.parse(decodeURI(body));
+    console.log(bodyAlt)
 
-    console.log(payload)
-
-    // purgeComment(id);
     callback(null, {
         statusCode: 200,
-        body: "sub create func worked! conga rat"
-        // body: "Comment deleted"
+        body: "sub create func worked! conga rat my dude"
     });
 
 }
