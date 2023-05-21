ad0c7368a88575a3cdd3b675cfdb52ed48cd76c4
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-21 18:00:05 -0400

    Make changes - submit and delete admin page functions - I THINK ITS WORKING NOW!!!!!!!!!!!

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -15,48 +15,36 @@ const {
 
 //ONLY RUN THE PURGE CALL WHEN DELETION-GRIDDY REACHES 10 LETTERS..???? or something
 
-function purgeComment(formID, id) {
-    var url = `https://api.netlify.com/api/v1/forms/${formID}/submissions?access_token=${NETLIFY_AUTH_TOKEN}/${id}`;
+async function purgeComment(formID, id) {
+    var url = `https://api.netlify.com/api/v1/forms/${formID}/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
 
-    return fetch(url, {method: "DELETE"})
+    await fetch(url, {method: "DELETE"})
         .then(r => console.log(r))
         .then(() => console.log("deletion successful!"))
         .catch((error) => console.log("Error Message: " + error));
 }
 //---------------------------------------------------------------------
 exports.handler = async (event) => {
-    // Get data off the event body
     const {
         theFormID,
         letterIDs
-    } = JSON.parse(event.body)
+    } = event.body
 
-    console.log("this is typeof letterIDs "+typeof letterIDs)
-    // letterIDs.forEach(purgeComment(theFormID))
-    async function doThis() {
-        for (const thisID in letterIDs) {
-            await purgeComment(theFormID, letterIDs[thisID])
+    const responses = [];
+
+    return async function doThis() {
+        for await (var thisID of letterIDs) {
+            responses.push(await purgeComment(theFormID, thisID))
         }
-        return "cool"
-    }
+        return responses
 
-    //below here is just log checking!
-
-    var bodyAlt = JSON.parse(event.body);
-
-    console.log(bodyAlt)
-
-    return doThis()
+    }()
         .then((res) => {
-            // Log the success into our function log
             console.log(`doThis function worked! ${res} `)
-            // return with a 200 status and a stringified JSON object we get from the Sanity API
             return { statusCode: 200, body: "this is so sick" };
         })
         .catch(err => {
-            // If there's an error, log it
-            // and return a 500 error and a JSON string of the error
-            console.log(err)
+            console.log("errormesage = " +err)
             return {
                 statusCode: 500, body: JSON.stringify(err)
             }
