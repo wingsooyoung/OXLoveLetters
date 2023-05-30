4eb29c94bcd6d98f5f956923c846a21018570eda
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-29 22:41:00 -0400

    Make changes - Fixing links and adding navigation menus for pagination!

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
     config.addShortcode("footer", function() {
         return `<footer>
         <p id="disclaimer"><small><em class="show">DISCLAIMER</em></small></p>
-        <p id="line1"><a href="/admin/approveletters" style="text-decoration: none;"><i class="ph-bold ph-key"></i></a> <u> 2023 </u><i class="ph ph-copyright"></i> <a href="https://twitter.com/oxmasterlist">OXMasterlist</a>
+        <p id="line1"><a href="/admin/page-1/" style="text-decoration: none;"><i class="ph-bold ph-key"></i></a> <u> 2023 </u><i class="ph ph-copyright"></i> <a href="https://twitter.com/oxmasterlist">OXMasterlist</a>
         / <a href="https://omegaxmasterlist.tumblr.com">omegaxmasterlist</a> <span>&bull; <a href="/pages/sitemap">SITEMAP</a> &bull; <a href="/pages/faq">FAQ</a> &bull; <a href="/pages/aboutthisproject/#contact">CONTACT ME</a></span>
         </p>
         <div class="identity-overlay"><div data-netlify-identity-button></div></div>
