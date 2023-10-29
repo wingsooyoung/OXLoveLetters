6676da2f43710763def5a49611563e23b383339c
wingsooyoung <hsm_rox_16@hotmail.com>
2023-10-29 21:55:23 +0000

    more changes and testing

--- src/site/mypage.jsx
+++ src/site/mypage.jsx
@@ -1,13 +1,60 @@
 import EleventyContext from 'eleventy-plugin-react-ssr/context';
 import { useContext } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+function BasicModal() {
+    const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
 
 function ContainerBox() {
     const { customData } = useContext(EleventyContext);
 
+    
+
     return (
         <div
             className="containerbox grid-item"
-            onClick="modalZoom(this)">
+            // onClick="modalZoom(this)"
+            onClick={handleOpen}
+            >
 
             <img
                 src={customData.source}
@@ -63,6 +110,7 @@ function MyPage() {
             <h1 className={customData.foo}>{title}</h1>
             <p>URL: {page.url}</p>
             <GridContainer />
+            <BasicModal />
         </>
     );
 }
