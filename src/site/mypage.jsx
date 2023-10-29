76c49c3e38d4a947c5f5284560c69e44bc23b957
wingsooyoung <hsm_rox_16@hotmail.com>
2023-10-29 21:15:50 +0000

    testing MyPage edits

--- src/site/mypage.jsx
+++ src/site/mypage.jsx
@@ -1,6 +1,59 @@
 import EleventyContext from 'eleventy-plugin-react-ssr/context';
 import { useContext } from 'react';
 
+function ContainerBox() {
+    const { customData } = useContext(EleventyContext);
+
+    return (
+        <div
+            className="containerbox grid-item"
+            onClick="modalZoom(this)">
+
+            <img
+                src={customData.src}
+                alt={customData.altText}
+            />
+                    
+            <div 
+                id="contentbox" 
+                className="template"> 
+                {customData.content}
+            </div>
+
+            <div 
+                id="namebox" 
+                className="template"> 
+                {customData.content}
+            </div>
+                
+            <div 
+                id="lettercount" 
+                className="template"> 
+                {customData.content}
+            </div>
+
+            <div 
+                id="membersbox" 
+                className="template" 
+                style="display: none"> 
+                {customData.content}
+            </div>
+        </div>
+    )
+}
+
+
+function GridContainer() {
+    return (
+        <div 
+            className="grid-container">
+            <ContainerBox />
+            <ContainerBox />
+            <ContainerBox />
+        </div>
+    )
+}
+
 function MyPage() {
     // Access to anything from the data cascade, including page data
     // and Eleventy supplied data from https://www.11ty.dev/docs/data-eleventy-supplied/
@@ -10,15 +63,20 @@ function MyPage() {
         <>
             <h1 className={customData.foo}>{title}</h1>
             <p>URL: {page.url}</p>
+            <GridContainer />
         </>
     );
 }
 
+//BELOW: template front matter, but for js/jsx files
 MyPage.data = {
-    layout: 'layouts/base.njk',
+    layout: 'layouts/base.njk', //THIS WORKS!!!!  I CAN USE REACT COMPENENTS ON MY PRE-STYLED BASE LAYOUT PAGE!!!
     title: 'Hello world',
     customData: {
         foo: 'bar',
+        source: '/assets/small borders postcard.svg',
+        altText: 'this is the image desc',
+        content: 'this is the placeholder text'
     },
 };
 
