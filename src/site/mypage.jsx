6424b7b4019a5292f20993dce71aaf1dc73130fa
wingsooyoung <hsm_rox_16@hotmail.com>
2023-10-30 01:35:48 +0000

    let's try this again!

--- src/site/mypage.jsx
+++ src/site/mypage.jsx
@@ -1,114 +1,92 @@
 import EleventyContext from 'eleventy-plugin-react-ssr/context';
 import { useContext } from 'react';
 import { useState } from 'react';
-import { createPortal } from 'react-dom';
 
-function MyInput() {
-  const [text, setText] = useState('hello');
+// function MyInput() {
+//   const [text, setText] = useState('hello');
 
-  function handleChange(e) {
-    setText(e.target.value);
-  }
+//   function handleChange(e) {
+//     setText(e.target.value);
+//   }
 
-  return (
-    <>
-      <input value={text} onChange={handleChange} />
-      <p>You typed: {text}</p>
-      <button onClick={() => setText('hello')}>
-        Reset
-      </button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <input value={text} onChange={handleChange} />
+//       <p>You typed: {text}</p>
+//       <button onClick={() => setText('hello')}>
+//         Reset
+//       </button>
+//     </>
+//   );
+// }
 
-function ModalContent({ onClose,  }) {
-  const [image, setImage] = useState('url.com');
-  const [altText, setAltText] = useState('alt placeholder');
-  const [content, setContent] = useState('content placeholder');
-  const [nickname, setNickname] = useState('nickname placeholder');
-  const [counter, setCounter] = useState('counter placeholder');
-
-
-  function handleChange(e) {
-    setImage(e.target.children[0].attributes[0].value)
-    setAltText(e.target.children[0].attributes[1].value)
-    setContent(e.target.children[1].textContent)
-    setNickname(e.target.children[2].textContent)
-    setCounter(e.target.children[3].textContent)
-
-  }
+function ModalContent({ onClose, image, altText, content, nickname, counter }) {
 
     return (
-    <div class="containerboxM grid-item modal-content" id="modal1" onClick={onClose}>
+    <div class="containerboxM grid-item modal-content" id="modal1" style={{visibility: {hidden}}}>
       <img src={image} alt={altText} id="postcardM" />
       <div id="contentboxM" class="template">{content}</div>
       <div id="nameboxM" class="template">{nickname}</div>
       <div id="lettercountM" class="template">{counter}</div>
+      <button onClick={onClose}>Close</button>
     </div>
     )
-  
-
-
-}
-// -------------------------- my modal --------------------------------
-
-//----------------------------------------------------------------------
-
-function PortalExample() {
-  const [showModal, setShowModal] = useState(false);
-  return (
-    <>
-      <button onClick={() => setShowModal(true)}>
-        Show modal using a portal
-      </button>
-      {showModal && createPortal(
-        <ModalContent onClose={() => setShowModal(false)} />,
-        document.body
-      )}
-    </>
-  );
 }
 
 function ContainerBox() {
     const { customData } = useContext(EleventyContext);
+    const [showModal, setShowModal] = useState(false);
+    const [image, setImage] = useState('placeholder.com');
+    const [altText, setAltText] = useState('alt placeholder');
+    const [content, setContent] = useState('content placeholder');
+    const [nickname, setNickname] = useState('nickname placeholder');
+    const [counter, setCounter] = useState('counter placeholder');
+  
+    function handleChange(e) {
+      setImage(e.target.children[0].attributes[0].value)
+      setAltText(e.target.children[0].attributes[1].value)
+      setContent(e.target.children[1].textContent)
+      setNickname(e.target.children[2].textContent)
+      setCounter(e.target.children[3].textContent)
 
+      (() => setShowModal(true))
+    }
     return (
+      <>
         <div
             className="containerbox grid-item"
-            // onClick="modalZoom(this)" INCORRECT!
-            // onClick={x}
             onClick={handleChange}
             >
-
             <img
                 src={customData.source}
                 alt={customData.altText}
             />
-                    
             <div 
                 id="contentbox" 
                 className="template"> 
                 {customData.content}
             </div>
-
             <div 
                 id="namebox" 
                 className="template"> 
                 {customData.content}
             </div>
-                
             <div 
                 id="lettercount" 
                 className="template"> 
                 {customData.content}
             </div>
-
             <div 
                 id="membersbox" 
                 className="template">
                 {customData.content}
             </div>
         </div>
+        {showModal && (
+          <ModalContent onClose={() => setShowModal(false)} image={image} altText={altText} content={content} nickname={nickname} counter={counter}/>
+        )}
+        
+      </>
     )
 }
 
@@ -117,10 +95,9 @@ function GridContainer() {
     return (
         <div 
             className="grid-container">
-            <ContainerBox onClick={handleChange}/>
-            <ContainerBox onClick={handleChange}/>
-            <ContainerBox onClick={handleChange}/>
-            <ModalContent />
+            <ContainerBox />
+            <ContainerBox />
+            <ContainerBox />            
         </div>
     )
 }
