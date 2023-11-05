faa76de6dbde08959085a704ba4c6607e861d447
wingsooyoung <hsm_rox_16@hotmail.com>
2023-11-05 18:21:03 -0500

    Adjusting the sanitytestpage

--- sanity.js
+++ sanity.js
@@ -1,4 +1,13 @@
-import client from '/client.js'
+
+const {createClient} = await import('https://esm.sh/@sanity/client')
+// const {createClient} = require('@sanity/client')
+
+const client = createClient({
+    projectId: 'o9bzeud3',
+    dataset: 'production',
+    useCdn: true, // set to `false` to bypass the edge cache
+    apiVersion: '2023-11-05', // use current date (YYYY-MM-DD) to target the latest API version
+})
 
 
 
